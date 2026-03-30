930b3c9ee0ff232d851866514e3e3fbfcde5b4c5
Dnyanprasad Patil <dnyaneshpatil799@gmail.com>
2026-03-30 11:26:10 +0530

    Update datauri.js

--- backend/utils/datauri.js
+++ backend/utils/datauri.js
@@ -7,5 +7,5 @@ const getDataUri = (file) => {
     const extName = path.extname(file.originalname).toString();
     return parser.format(extName, file.buffer).content;
 }
-
+//
 module.exports = getDataUri;
