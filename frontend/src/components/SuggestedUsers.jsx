ffd804031f28ca9bfa903e217c82b48a6e5353ac
Dnyanprasad Patil <dnyaneshpatil799@gmail.com>
2026-03-30 11:38:20 +0530

    Update SuggestedUsers.jsx

--- frontend/src/components/SuggestedUsers.jsx
+++ frontend/src/components/SuggestedUsers.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'//
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import { Link } from 'react-router-dom'
 
