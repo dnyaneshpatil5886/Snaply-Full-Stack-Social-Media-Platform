b45fd373aab733f64573ddae273f9c0ef97f6ff2
Dnyanprasad Patil <dnyaneshpatil799@gmail.com>
2026-03-30 11:18:27 +0530

    Update message.routes.js

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -6,4 +6,4 @@ const route = express.Router();
 route.post('/send/:id', isAuthenticated, sendMessage);
 route.get('/all/:id', isAuthenticated, getMessages);
 
-module.exports = route;
+module.exports = route;//
