74151ac1326a471aed31c709f5afea27aaacfb06
Dnyanprasad Patil <dnyaneshpatil799@gmail.com>
2026-03-30 11:37:12 +0530

    Update avatar.jsx

--- frontend/src/components/ui/avatar.jsx
+++ frontend/src/components/ui/avatar.jsx
@@ -9,7 +9,7 @@ const Avatar = React.forwardRef(({ className, ...props }, ref) => (
     className={cn("relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full", className)}
     {...props} />
 ))
-Avatar.displayName = AvatarPrimitive.Root.displayName
+Avatar.displayName = AvatarPrimitive.Root.displayName//
 
 const AvatarImage = React.forwardRef(({ className, ...props }, ref) => (
   <AvatarPrimitive.Image
